7d8e250cd6dea6c2df11556b43fbf6e7f7d165dc
Drew Hubley <dahubley@hotmail.ca>
2023-11-22 17:47:32 -0400

    pre-commit

--- include/xtensor-signal/fft.hpp
+++ include/xtensor-signal/fft.hpp
@@ -1,49 +1,43 @@
 
 #ifdef XTENSOR_USE_TBB
-    #include <oneapi/tbb.h>
+#include <oneapi/tbb.h>
 #endif
-#include <xtensor/xview.hpp>
 #include <xtensor/xarray.hpp>
 #include <xtensor/xbuilder.hpp>
+#include <xtensor/xview.hpp>
 
 #include <stdexcept>
 
-namespace xt::fft
-{
-    template<class E>
-    inline auto fft(E&& e)
-    {
-        using namespace xt::placeholders;
-        using namespace std::complex_literals;
-        auto N = e.size();
-        auto pi = xt::numeric_constants<double>::PI;
-        auto ev = xt::eval(e);
-        if (N <= 1)
-        {
-            return ev;
-        }
-        else
-        {
-    #ifdef XTENSOR_USE_TBB
-            xt::xarray<std::complex<float>> even;
-            xt::xarray<std::complex<float>> odd;
-            oneapi::tbb::parallel_invoke([&]{even = fft(xt::view(ev, xt::range(0, _, 2))); },
-                [&]{odd = fft(xt::view(ev, xt::range(1, _, 2)));});
-    #else
-            auto even = fft(xt::view(ev, xt::range(0, _, 2)));
-            auto odd = fft(xt::view(ev, xt::range(1, _, 2)));
-    #endif
+namespace xt::fft {
+template <class E> inline auto fft(E &&e) {
+  using namespace xt::placeholders;
+  using namespace std::complex_literals;
+  auto N = e.size();
+  auto pi = xt::numeric_constants<double>::PI;
+  auto ev = xt::eval(e);
+  if (N <= 1) {
+    return ev;
+  } else {
+#ifdef XTENSOR_USE_TBB
+    xt::xarray<std::complex<float>> even;
+    xt::xarray<std::complex<float>> odd;
+    oneapi::tbb::parallel_invoke(
+        [&] { even = fft(xt::view(ev, xt::range(0, _, 2))); },
+        [&] { odd = fft(xt::view(ev, xt::range(1, _, 2))); });
+#else
+    auto even = fft(xt::view(ev, xt::range(0, _, 2)));
+    auto odd = fft(xt::view(ev, xt::range(1, _, 2)));
+#endif
 
-            auto range = xt::arange<double>(N / 2);
-            auto exp = xt::exp(-2i * pi * range / N);
-            auto t = exp * odd;
-            auto first_half = even + t;
-            auto second_half = even - t;
-            auto spectrum = xt::xtensor<std::complex<float>, 1>::from_shape({N});
-            xt::view(spectrum, xt::range(0, N / 2)) = first_half;
-            xt::view(spectrum, xt::range(N / 2, N)) = second_half;
-            return spectrum;
-        }
-    }
+    auto range = xt::arange<double>(N / 2);
+    auto exp = xt::exp(-2i * pi * range / N);
+    auto t = exp * odd;
+    auto first_half = even + t;
+    auto second_half = even - t;
+    auto spectrum = xt::xtensor<std::complex<float>, 1>::from_shape({N});
+    xt::view(spectrum, xt::range(0, N / 2)) = first_half;
+    xt::view(spectrum, xt::range(N / 2, N)) = second_half;
+    return spectrum;
+  }
 }
-
+} // namespace xt::fft
--- test/fft_test.cpp
+++ test/fft_test.cpp
@@ -2,19 +2,10 @@
 #include "doctest/doctest.h"
 #include "xtensor-signal/fft.hpp"
 
-
 TEST_SUITE("fft") {
 
-  TEST_CASE("fft_single") {
-
-  }
-  TEST_CASE("fft_double") {
-
-  }
-  TEST_CASE("fft_csingle") {
-
-  }
-  TEST_CASE("fft_cdouble") {
-
-  }
+  TEST_CASE("fft_single") {}
+  TEST_CASE("fft_double") {}
+  TEST_CASE("fft_csingle") {}
+  TEST_CASE("fft_cdouble") {}
 }

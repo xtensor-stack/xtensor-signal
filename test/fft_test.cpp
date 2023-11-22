848e984525cc0fde0c42a5e5ac394535de574c6e
Drew Hubley <dahubley@hotmail.ca>
2023-11-22 17:46:04 -0400

    Added basic fft more work needed

---
+++ test/fft_test.cpp
@@ -0,0 +1,20 @@
+
+#include "doctest/doctest.h"
+#include "xtensor-signal/fft.hpp"
+
+
+TEST_SUITE("fft") {
+
+  TEST_CASE("fft_single") {
+
+  }
+  TEST_CASE("fft_double") {
+
+  }
+  TEST_CASE("fft_csingle") {
+
+  }
+  TEST_CASE("fft_cdouble") {
+
+  }
+}
